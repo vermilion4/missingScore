440659972543652f158d4a7ed2824f84d5c7943c
Adaeze Ndupu <andupu4@gmail.com>
2022-08-27 21:33:58 +0100

    Add comments

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-let scores = [45, 50, 55, 70, 75, 80, 85, 90, 95]
-let index, missing;
+//Test scores
+let scores = [45, 50, 55, 65, 70, 75, 80, 85, 90, 95];
 
-function missingScore(arr){
-  let diff = arr[0]-arr[1]
+function missingScore(arr) {
+  //difference between first 2 scores
+  let diff = arr[0] - arr[1];
+  let index, missing;
 
-  for (let i=0; i < arr.length; i++){
-    if (arr[i]-arr[i+1] !== diff && i+1 !== arr.length){
-      index = arr.indexOf(arr[i+1])
-      missing = arr[i+1] + diff
+  //loop through scores
+  for (let i = 0; i < arr.length; i++) {
+    // if the difference between any 2 scores is different from the difference between the first 2 scores and is not out of bound
+    if (arr[i] - arr[i + 1] !== diff && i + 1 !== arr.length) {
+      //index becomes the position of the second score
+      index = i + 1;
+      //the missing score is gotten from using the original difference with the second score
+      missing = arr[i + 1] + diff;
     }
   }
+  // arr.splice(index,0,missing)
+  // console.log(arr)
+
+  //result
+  return `The missing score is ${missing} found at position ${index}`;
 }
 
-missingScore(scores)
-
-
-// arr.splice(index,0,missing)
-// console.log(arr)
-
-console.log(`The missing score is ${missing} found at position ${index}`)
+//Function call
+console.log(missingScore(scores));
