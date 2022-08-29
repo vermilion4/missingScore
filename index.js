5a76c4b7ef4e659202a2fc28a7ebc3f964e5b13b
Adaeze Ndupu <andupu4@gmail.com>
2022-08-29 17:14:58 +0100

    update for known range of scores

--- index.js
+++ index.js
@@ -3,25 +3,24 @@ var scores = [45, 50, 55, 65, 70, 75, 80, 85, 90, 95];
 
 function missingScore(arr) {
     'use strict';
-    //difference between first 2 scores and other bindings
-    var diff = arr[0] - arr[1],
+    //difference between known scores and other bindings
+    var last = arr.length - 3,
+        diff = arr[last] - arr[last + 1],
         index,
         missing,
         i;
 
     //loop through scores
-    for (i = 0; i < arr.length; i += 1) {
-      // if the difference between any 2 scores is different from the difference between the first 2 scores and is not out of bound
-        if (arr[i] - arr[i + 1] !== diff && i + 1 !== arr.length) {
+    for (i = 0; i < last; i += 1) {
+      // if the difference between any 2 scores is different from the diff
+        if (arr[i] - arr[i + 1] !== diff) {
             index = i + 1;
             missing = arr[i + 1] + diff;
         }
     }
-    // arr.splice(index,0,missing)
-    // console.log(arr)
 
     //result
-    return 'The missing score is ' + missing + ' found at position' + index;
+    return 'The missing score is ' + missing + ' found at position ' + index;
 }
 
 //Function call
