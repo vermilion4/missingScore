fbbfb1e88530e43870a191c567d76d110781e1b0
Adaeze Ndupu <andupu4@gmail.com>
2022-08-29 15:50:26 +0100

    EsLint

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 //Test scores
-let scores = [45, 50, 55, 65, 70, 75, 80, 85, 90, 95];
+var scores = [45, 50, 55, 65, 70, 75, 80, 85, 90, 95];
 
 function missingScore(arr) {
-  //difference between first 2 scores
-  let diff = arr[0] - arr[1];
-  let index, missing;
+    'use strict';
+    //difference between first 2 scores and other bindings
+    var diff = arr[0] - arr[1],
+        index,
+        missing,
+        i;
 
-  //loop through scores
-  for (let i = 0; i < arr.length; i++) {
-    // if the difference between any 2 scores is different from the difference between the first 2 scores and is not out of bound
-    if (arr[i] - arr[i + 1] !== diff && i + 1 !== arr.length) {
-      //index becomes the position of the second score
-      index = i + 1;
-      //the missing score is gotten from using the original difference with the second score
-      missing = arr[i + 1] + diff;
+    //loop through scores
+    for (i = 0; i < arr.length; i += 1) {
+      // if the difference between any 2 scores is different from the difference between the first 2 scores and is not out of bound
+        if (arr[i] - arr[i + 1] !== diff && i + 1 !== arr.length) {
+            index = i + 1;
+            missing = arr[i + 1] + diff;
+        }
     }
-  }
-  // arr.splice(index,0,missing)
-  // console.log(arr)
+    // arr.splice(index,0,missing)
+    // console.log(arr)
 
-  //result
-  return `The missing score is ${missing} found at position ${index}`;
+    //result
+    return 'The missing score is ' + missing + ' found at position' + index;
 }
 
 //Function call
